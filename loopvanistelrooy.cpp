4c58090b324ddffe4d77b83f47176398b9d42da6
jwkim628 <jwkim628@hanyang.ac.kr>
2025-03-06 22:03:46 +0900

    sunloop

--- loopvanistelrooy.cpp
+++ loopvanistelrooy.cpp
@@ -3,9 +3,13 @@ using namespace std;
 
 int main()
 {
-    char my_string[]="Hello \0, World!";
+    char my_string[]="Hello\0, World!";
     for(int i=0;i<sizeof(my_string);i++)
     {
+        if(my_string[i]=='\0')
+        {
+            break;
+        }
         cout << my_string[i];
     }
     return 0;
