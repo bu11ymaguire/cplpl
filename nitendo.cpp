1d66964bff27defdaeb72c5283c83e8b263c8627
jwkim628 <jwkim628@hanyang.ac.kr>
2025-03-06 18:05:32 +0900

    switch branch

--- nitendo.cpp
+++ nitendo.cpp
@@ -6,8 +6,15 @@ int main()
     int a;
     cout << "enter integer:";
     cin >> a;
-    cout <<a <<" is " << (a % 2 ==0 ? "even" : "odd") << endl; /*삼항연산자 
-    (조건식 ? 참의 결과값 : 거짓의 결과값)*/
+    switch(a%2)
+    {
+        case 0:
+        cout << "Even number."<<endl;
+        break;
+        case 1:
+        cout << "Odd number."<<endl;
+        break;
+    }
     
     return 0;
 }
