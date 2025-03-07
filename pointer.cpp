b5a9dd936d78664dd4ba1abc1c12069c1264f6eb
jwkim628 <jwkim628@hanyang.ac.kr>
2025-03-07 11:08:16 +0900

    stannum

--- pointer.cpp
+++ pointer.cpp
@@ -6,14 +6,20 @@ int main()
     int a =10;
     int *b = &a;
     cout << b << endl; //a의 메모리주소
+    cout << &a << endl;
     cout << a << endl; //10
     cout << *b << endl; //b가 가리키는 메모리 주소의 저장된 값 == 10
-    cout << size_t(b) <<" " << size_t(b+1) <<endl;
     cout << size_t(b) << endl;
     b++;
-    cout << b << endl;
-    cout << a << endl; 
-    cout << *b << endl; 
-    cout << size_t(b) <<" " << size_t(b+1) <<endl;
+    cout << "Phase2" <<endl;
+    if (b==&a)
+    {
+        cout << a << endl;
+    }
+    else
+    {
+        cout << size_t(b) <<" "<< size_t(&a) << endl;
+        cout << *b<<" "<< a << endl;
+    }
     return 0;
 }
