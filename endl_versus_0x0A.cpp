17cc7e658d2b199eccf22924a2d7c71911eb6ea8
jwkim628 <jwkim628@hanyang.ac.kr>
2025-03-07 10:08:34 +0900

    endl_versus_0x0A

--- endl_versus_0x0A.cpp
+++ endl_versus_0x0A.cpp
@@ -3,5 +3,6 @@ using namespace std;
 
 int main()
 {
-    cout << "try again\n";
+    cout << "try again\n"; //메모리 덜 사용 But 출력 시간이 걸림
+    cout << "try again" << endl; //즉시 출력 But 출력 버퍼를 비우는 데 자원을 소모
 }
