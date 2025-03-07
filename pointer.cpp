b88ee252cc8781e473085e86df39072a5b31be59
jwkim628 <jwkim628@hanyang.ac.kr>
2025-03-07 11:00:11 +0900

    msft_bill_gates

--- pointer.cpp
+++ pointer.cpp
@@ -5,14 +5,15 @@ int main()
 {
     int a =10;
     int *b = &a;
-    cout << b << endl; 
-    cout << a << endl;
-    cout << *b << endl;
-    cout << b <<" " << b+1 <<endl;
-    cout << b << endl;
+    cout << b << endl; //a의 메모리주소
+    cout << a << endl; //10
+    cout << *b << endl; //b가 가리키는 메모리 주소의 저장된 값 == 10
+    cout << size_t(b) <<" " << size_t(b+1) <<endl;
+    cout << size_t(b) << endl;
     b++;
     cout << b << endl;
-    cout << a << endl;
-    cout << *b << endl;
-    cout << b <<" " << b+1 <<endl;
+    cout << a << endl; 
+    cout << *b << endl; 
+    cout << size_t(b) <<" " << size_t(b+1) <<endl;
+    return 0;
 }
