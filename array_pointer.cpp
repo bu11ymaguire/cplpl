4604562247cbb1a82fbdeb1b6bda2f6eb6dbf6c6
jwkim628 <jwkim628@hanyang.ac.kr>
2025-03-08 13:37:38 +0900

    array_is_a_pointer

--- array_pointer.cpp
+++ array_pointer.cpp
@@ -7,12 +7,12 @@ int main()
     char *H = hello;
     for(int i =0;i<=sizeof(hello);i++)
     {
-        cout << hello[i];
+        cout << hello[i]; //문자열의 [0]부터 [sizeof(array)]까지의 원소를 차례대로 출력.
     }
     cout <<endl;
     for (int i =0;i<=sizeof(hello);i++)
     {
-        cout << *(H+i);
+        cout << *(H+i); //*(H+i)는 hello[i]의 주소가 가리키는 내용을 가져온다.
     }
-    return 0;
+    return 0; 
 }
